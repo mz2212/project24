7f54f3a4b6fda7c54be4017efa3b389d30fb3783
Mike <michaelpulliam1@gmail.com>
2017-07-04 23:54:42 -0600

    Get top level comments working

--- main.go
+++ main.go
@@ -4,42 +4,45 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
 	"time"
 )
 
 type post struct {
-	ThreadID   int
 	Name       string
 	Subject    string
 	Body       string
 	TimePosted time.Time
+	Comments   comments
+	ThreadID   int
 }
 
-type posts []post
+// I'm aware I could just use the `posts` type everywhere,
+// but this is easier in my head.
+type posts map[int]post
+type comments map[int]post
 
 func main() {
 	var (
-		p posts
+		p = make(posts)
 	)
 	p.newPost(1, "Anon", "Test Post", "Test Post body that is a lot of words")
 	http.HandleFunc("/", p.viewPosts)
 	http.HandleFunc("/newthread/", p.newThread)
+	http.HandleFunc("/reply/", p.reply)
 	http.ListenAndServe(":8080", nil)
 }
 
-func (p *posts) newPost(threadID int, name, subject, body string) {
-	*p = append(*p, post{ThreadID: threadID, Name: name, Subject: subject, Body: body, TimePosted: time.Now()})
-}
-
 // I know deleting posts before viewing probably isn't the best way to do it, but it's better than polling
+// Actually, polling every half hour or so might be better...
 func (p *posts) viewPosts(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("index.html")
 	if err != nil {
 		fmt.Println("Failed to load template: ", err)
 	}
-	for i, po := range *p { // The line below is what needs to be changed to change the time posts live
+	for key, po := range *p { // The line below is what needs to be changed to change the time posts live
 		if time.Since(po.TimePosted) >= 24*time.Hour {
-			*p = (*p)[:i+copy((*p)[i:], (*p)[i+1:])] // This is weird, but it works
+			delete(*p, key)
 		}
 	}
 	t.Execute(w, p)
@@ -48,4 +51,25 @@ func (p *posts) viewPosts(w http.ResponseWriter, r *http.Request) {
 func (p *posts) newThread(w http.ResponseWriter, r *http.Request) {
 	p.newPost(len(*p)+1, r.FormValue("name"), r.FormValue("subject"), r.FormValue("body"))
 	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func (p *posts) reply(w http.ResponseWriter, r *http.Request) {
+	threadID, err := strconv.Atoi(r.URL.Path[len("/reply/"):])
+	if err != nil {
+		fmt.Println("Failed attempt to post comment: ", err)
+	}
+	p.newComment(threadID, r.FormValue("name"), r.FormValue("body"))
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func (p *posts) newPost(threadID int, name, subject, body string) {
+	(*p)[threadID] = post{ThreadID: threadID, Name: name, Subject: subject, Body: body, TimePosted: time.Now(), Comments: make(comments)}
+}
+
+func (p *posts) newComment(threadID int, name, body string) {
+	(*p)[threadID].Comments[len((*p)[threadID].Comments)+1] = post{ThreadID: threadID, Name: name, Subject: (*p)[threadID].Subject, Body: body, TimePosted: time.Now(), Comments: make(comments)}
+}
+
+func (c *comments) newComment(threadID int, name, body string) {
+	(*c)[threadID].Comments[len((*c)[threadID].Comments)+1] = post{ThreadID: threadID, Name: name, Subject: (*c)[threadID].Subject, Body: body, TimePosted: time.Now(), Comments: make(comments)}
 }
